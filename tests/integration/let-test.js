03f3fa9c26c3d3d9ac9ad2a463b14a414d4d5d45
Taras Mankovski <tarasm@gmail.com>
2016-10-12 16:29:57 -0400

    Refactored the integration test to eliminate side effects in test

--- tests/integration/let-test.js
+++ tests/integration/let-test.js
@@ -23,43 +23,58 @@ describeComponent('let', 'Integration: let helper', {
 },
   function() {
 
-    if (emberVersionIs('lessThan', "2.0.0")) {
-      return ;
-    }
+    describe('inline', function() {
+      if (emberVersionIs('lessThan', "2.0.0")) {
+        return ;
+      }
+      
+      describe('context', function() {
+        beforeEach(function() {
+          this.set('name', 'Alice');
+          this.render(hbs`{{let name="Bob"}}`);
+        });
 
-    it('does not mutate the context', function(){
-      this.set('name', 'Alice');
-      this.render(hbs`{{let name="Bob"}}`);
-      expect(this.get('name')).to.eq('Alice');
-    });
+        it('does not mutate the context', function(){
+          expect(this.get('name')).to.eq('Alice');
+        });
+      });
 
-    it('works correctly in a loop', function(){
-      this.set('pets', [{ type: 'cat' }, { type: 'dog' }, { type: 'pig' }]);
-      this.render(hbs`
-        <span class="before">{{type}}</span>
-        {{#each pets as |pet|}}
-          {{let type=pet.type}}
-          <span class="item">{{type}}</span>
-        {{/each}}
-        <span class="after">{{type}}</span>
-      `);
+      describe('in loop', function() {
+        beforeEach(function() {
+          this.set('pets', [{ type: 'cat' }, { type: 'dog' }, { type: 'pig' }]);
+          this.render(hbs`
+            <span class="before">{{type}}</span>
+            {{#each pets as |pet|}}
+              {{let type=pet.type}}
+              <span class="item">{{type}}</span>
+            {{/each}}
+            <span class="after">{{type}}</span>
+          `);
+        });
 
-      expect(this.$('.before').text()).to.eq('');
-      expect(this.$('.after').text()).to.eq('');
-      expect(this.$('.item').text()).to.eq('catdogpig');
-    });
+        it('works correctly in a loop', function(){
+          expect(this.$('.before').text()).to.eq('');
+          expect(this.$('.after').text()).to.eq('');
+          expect(this.$('.item').text()).to.eq('catdogpig');
+        });
+      });
 
-    it('creates bindings in parallel', function(){
-      this.set('pets', [{ type: 'cat' }, { type: 'dog' }, { type: 'pig' }]);
-      this.render(hbs`
-        {{let a='a' b='b'}}
-        <span class="before">{{a}} {{b}}</span>
-        {{let a='A' b='B' c=(concat a b)}}
-        <span class="after">{{a}} {{b}} {{c}}</span>
-      `);
+      describe('multiple', function() {
+        beforeEach(function() {
+          this.set('pets', [{ type: 'cat' }, { type: 'dog' }, { type: 'pig' }]);
+          this.render(hbs`
+            {{let a='a' b='b'}}
+            <span class="before">{{a}} {{b}}</span>
+            {{let a='A' b='B' c=(concat a b)}}
+            <span class="after">{{a}} {{b}} {{c}}</span>
+          `);
+        });
 
-      expect(this.$('.before').text()).to.eq('a b');
-      expect(this.$('.after').text()).to.eq('A B ab');
+        it('creates bindings in parallel', function(){
+          expect(this.$('.before').text()).to.eq('a b');
+          expect(this.$('.after').text()).to.eq('A B ab');
+        });
+      });
     });
 
     describe('yield', function() {
